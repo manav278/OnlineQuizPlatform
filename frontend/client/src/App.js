d7d770823f3abe0c455bb4bfd574fe74015eb543
Manav <patelmanav278@gmail.com>
2024-02-17 00:29:54 +0530

    Quiz.js Component Updated and different quiz components added

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Quiz from './components/Quiz';
 
 function App() {
   return (
     <div>
-      <h1>Quiz</h1>
+      <Quiz></Quiz>
     </div>
   );
 }
--- frontend/client/src/components/Quiz.js
+++ frontend/client/src/components/Quiz.js
@@ -1,9 +1,86 @@
-import React from 'react'
-
+import React from "react";
+import math from "./math.jpg";
+import science from "./science.jpg";
 export default function Quiz() {
   return (
     <div>
-      <h1>Quiz</h1>
+      <div className="container">
+        <div className="row g-5">
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img src={math} className="image" alt="Math"></img>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img src={science} className="image" alt="Science"></img>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img src={science} className="image" alt="Science"></img>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img src={science} className="image" alt="Science"></img>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px",
+              }}
+            >
+              <img src={science} className="image" alt="Science"></img>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div
+              style={{
+                height: "50vh",
+                border: "2px solid black",
+                overflow: "hidden",
+                borderRadius: "8px"
+              }}
+            >
+              <img src={science} className="image" alt="Science"></img>
+              <h3>Science Quiz</h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
