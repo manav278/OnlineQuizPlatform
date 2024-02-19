2ebbe77e0191a827e369a3d05be7eed437a33b2b
Manav <patelmanav278@gmail.com>
2024-02-19 19:53:40 +0530

    [Updated]: Quiz.js code updated.

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import Quiz from './components/Quiz';
+import logo from "./logo.svg";
+import "./App.css";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
--- frontend/client/src/components/Quiz.js
+++ frontend/client/src/components/Quiz.js
@@ -1,84 +1,68 @@
 import React from "react";
-import math from "./math.jpg";
-import science from "./science.jpg";
-export default function Quiz() {
+import math from "./assets/math.jpg";
+import science from "./assets/science.jpg";
+import business from "./assets/business.jpg";
+import sports from "./assets/sports.jpg";
+import history from "./assets/history.jpg";
+import geography from "./assets/geography.jpg";
+import architecture from "./assets/architecture.jpg";
+import technology from "./assets/technology.jpg";
+export default function Com() {
+  const obj = [
+    {
+      title: "Math Quiz",
+      path: math,
+    },
+    {
+      title: "History Quiz",
+      path: history,
+    },
+    {
+      title: "Science Quiz",
+      path: science,
+    },
+    {
+      title: "Geography Quiz",
+      path: geography,
+    },
+    {
+      title: "Sports Quiz",
+      path: sports,
+    },
+    {
+      title: "Architecture Quiz",
+      path: architecture,
+    },
+    {
+      title: "Technology Quiz",
+      path: technology,
+    },
+    {
+      title: "Business Quiz",
+      path: business,
+    },
+  ];
   return (
     <div>
-      <div className="container">
-        <div className="row g-5">
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px",
-              }}
-            >
-              <img src={math} className="image" alt="Math"></img>
+      <div className="container-fluid text-light">
+        <div className="row g-3">
+          {/* --------------------------------------- */}
+          {obj.map((ob, index) => (
+            <div className="col-6 col-md-4 col-lg-3">
+              <div
+                className="bg-dark"
+                style={{
+                  height: "50vh",
+                  border: "2px solid black",
+                  overflow: "hidden",
+                  borderRadius: "8px",
+                }}
+              >
+                <img src={ob.path} className="image" alt="Math"></img>
+                <h3>{ob.title}</h3>
+              </div>
             </div>
-          </div>
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px",
-              }}
-            >
-              <img src={science} className="image" alt="Science"></img>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px",
-              }}
-            >
-              <img src={science} className="image" alt="Science"></img>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px",
-              }}
-            >
-              <img src={science} className="image" alt="Science"></img>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px",
-              }}
-            >
-              <img src={science} className="image" alt="Science"></img>
-            </div>
-          </div>
-          <div className="col-md-3">
-            <div
-              style={{
-                height: "50vh",
-                border: "2px solid black",
-                overflow: "hidden",
-                borderRadius: "8px"
-              }}
-            >
-              <img src={science} className="image" alt="Science"></img>
-              <h3>Science Quiz</h3>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
