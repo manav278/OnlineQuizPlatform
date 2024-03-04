217c668cf10306aa49ab636e5a0682608961ee05
Manav <patelmanav278@gmail.com>
2024-03-04 09:32:30 +0530

    [Updated]: Howit.js updated and code refactored.

--- frontend/client/src/components/Howit.js
+++ frontend/client/src/components/Howit.js
@@ -1,36 +1,39 @@
 import React from "react";
 import MainNav from "./MainNav";
 import howit from "./assets/Howit.jpg";
+import Footer from "./Footer"
 
 export default function Howit() {
   return (
     <div>
       <MainNav></MainNav>
-      <div class="container text-light">
+      <div class="container text-light raleway-font">
         <div class="row justify-content-around">
-          <div class="col-5">
+          <div class="col-md-6 order-md-first order-last" style={{padding:"5%"}}>
             <div className="row">
-              <div className="col-12">
+              <div className="col-12 text-primary">
                 <h1>Online</h1>
               </div>
-              <div className="col-12">
+              <div className="col-12 text-primary">
                 <h1>Quiz Platform</h1>
               </div>
-              <div className="col-12 my-3">
-                <p>
+              <div className="col-12 my-1">
+                <h4>
                   Welcome to QuizMania. Seamless Skills and knowledge evaluation
                   solution. Enjoy dynamic quizzing from anywhere in the world.
                   Conduct Virtual Quizzes With Our Secure and Robust Examination
                   Platform.
-                </p>
+                </h4>
               </div>
             </div>
           </div>
-          <div class="col-5 bg-danger">
-            <img src={howit}></img>
+          <div class="col-md-6  order-md-last order-first" style={{padding:"2%"}}>
+            <img src={howit} className="img-fluid"></img>
+            {/* 655*381 */}
           </div>
         </div>
       </div>
+      <Footer></Footer>
     </div>
   );
 }
